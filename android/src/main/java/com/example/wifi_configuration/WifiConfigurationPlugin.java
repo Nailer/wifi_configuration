917d0c8669b6196a541d1f3c828e238b164e2f22
Daniel Fischer <daniel.fischer@turftank.com>
2020-10-21 11:13:42 +0200

    Adds logging

--- android/src/main/java/com/example/wifi_configuration/WifiConfigurationPlugin.java
+++ android/src/main/java/com/example/wifi_configuration/WifiConfigurationPlugin.java
@@ -231,12 +231,14 @@ public class WifiConfigurationPlugin implements MethodCallHandler {
     }
 
 
-    private static void disconnectFromWifi(final String SSID){
-        Log.e("WifiUtils", "Disconnecting function hit");
+    private static void disconnectFromWifi(String SSID){
+        Log.e("WifiUtils", "Disconnecting function hit with SSID:" + SSID);
+        Log.e("LENGTH", SSID.length());
         WifiManager wifiManager = (WifiManager) Constant.context.getSystemService (Context.WIFI_SERVICE);
         List<WifiConfiguration> list = wifiManager.getConfiguredNetworks();
         for( WifiConfiguration i : list ) {
             Log.e("WifiUtils", "This SSID " + i.SSID);
+            Log.e("LENGTH", i.SSID.length());
             if(i.SSID == SSID) {
             Log.e("WifiUtils", "Disconnecting from: " + i.SSID);
             wifiManager.disableNetwork(i.networkId);
